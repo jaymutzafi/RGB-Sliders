fc0690dbfbb786b5d03267825fd093039e5490be
Paradox <contact@paradoxdesign.co>
2016-11-11 22:00:31 -0800

    Added data saving via UserDefaults

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -18,7 +18,12 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let defaults = UserDefaults.standard
+        redSlider.value = defaults.float(forKey: "red")
+        greenSlider.value = defaults.float(forKey: "green")
+        blueSlider.value = defaults.float(forKey: "blue")
+        
         colorSquare.layer.borderColor = UIColor.black.cgColor
         colorSquare.layer.borderWidth = 1
         updateBackground()
@@ -31,6 +36,19 @@ class ViewController: UIViewController {
 
     @IBAction func updateBackground() {
         colorSquare.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        
+        let defaults = UserDefaults.standard
+        defaults.set(redSlider.value, forKey: "red")
+        defaults.set(greenSlider.value, forKey: "green")
+        defaults.set(blueSlider.value, forKey: "blue")
+        defaults.synchronize()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "openColor" {
+            let colorController = segue.destination
+            colorController.view.backgroundColor = colorSquare.backgroundColor
+        }
     }
 }
 
